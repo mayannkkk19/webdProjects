87b05ab427261bb8ae8ec5de1b2e4cb69bdd5e66
mayankyadav19 <hatmario06@gmail.com>
2026-02-24 23:13:28 +0530

    used-jasmine-test-library

---
+++ javascript-amazon-project-main/tests/moneyTest.js
@@ -0,0 +1,13 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+if(formatCurrency(2095) === '20.95'){
+    console.log("Passed");
+}else{
+    console.log("Failed");
+}
+
+if(formatCurrency(2000.5) === '20.01'){
+    console.log("Passed");
+}else{
+    console.log("Failed");
+}
